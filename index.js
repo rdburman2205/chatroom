522aaf4d16aff53c532a3c0c145fc0efca6c90fa
rdburman2205 <rdburman2205@gmail.com>
2019-12-08 11:52:45 +0530

    initial

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var users = [];
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+	socket.on('new-user', function(name){
+		users[socket.id] = name
+		socket.broadcast.emit('user-connected', name)
+	})
+
+	socket.on('chat-message', function(msg){
+		io.emit('chat-message', {
+			message:msg,
+			sender:users[socket.id]
+		});
+	});
 });
 
 http.listen(port, function(){
-  console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
